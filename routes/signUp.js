4256ea80fad3d8004f9140db5650714f5f0ae07d
maurya20 <mukesh.ice17@gmail.com>
2020-11-17 14:49:57 +0530

    Jwt token created

--- routes/signUp.js
+++ routes/signUp.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const userSchema = require('../models/User')
 const User = mongoose.model("User")
+const jwt = require('jsonwebtoken')
 
 
 
@@ -11,10 +12,12 @@ router.post('/signup', async (req, res)=>{
     try {
     const user = new User({email, password})
     await user.save()
+    const token = jwt.sign({ userId:user._id }, 'my_secret_key')
+    res.send({ token })
     return res.status(200).send("A new user registered successfully")
 }catch (err) {
     return res.status(422).send(err.message)
-}
+} 
 })
 
 module.exports = router
