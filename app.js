28cd7fbf36812563a02b39fd2c4147230dbbd3ad
maurya20 <mukesh.ice17@gmail.com>
2020-11-17 14:24:56 +0530

    user registration error handling

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signupRouter = require('./routes/signUp')
+require('./models/User')
+
 
 var app = express();
+
+const db = 'mongodb://localhost:27017/rfq'
+mongoose.connect(db, { useUnifiedTopology: true, useCreateIndex:true, useNewUrlParser: true })
+mongoose.connection.on('error', err => debug(`MongoDB connection error: ${err}`));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', signupRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/signUp.js
+++ routes/signUp.js
@@ -8,9 +8,13 @@ const User = mongoose.model("User")
 
 router.post('/signup', async (req, res)=>{
     const {email, password} = req.body
+    try {
     const user = new User({email, password})
     await user.save()
-    res.send("You send a post request")
+    return res.status(200).send("A new user registered successfully")
+}catch (err) {
+    return res.status(422).send(err.message)
+}
 })
 
 module.exports = router
