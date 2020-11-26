2fc7fb720d63abd1d9f6c47a7dee5080fad0aab1
maurya20 <mukesh.ice17@gmail.com>
2020-11-26 11:38:31 +0530

    Products rendered on home page RFQ button added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,60 @@
-import React, { useState, useContext } from "react";
-import {RfqContext} from './RfqContext'
+import React, {useEffect,useState} from 'react';
+import {Link} from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import axios from 'axios'
 
-const Home = () => {
- 
-  const [appState, setAppState] = useContext(RfqContext)
-  return (
-    <div className='page container'>
-    <h3>Home page</h3>
-  <h1>{appState.logged?"LoggedIn":"NotLoggedIn"}</h1>
-  <h1>{appState.username}</h1>
-    </div>
-  )
-}
 
-export default Home
+
+const Home =()=>{
+   const [data, setData] = useState([])
+   console.log(data)
+   useEffect(() => {
+    axios.get('http://localhost:4200/api/products',
+    ).then(res => 
+      setData(res.data)
+      
+    
+    ).catch(function(error) {
+      return
+    });
+      
+  }
+  , []);
+    
+  
+        
+      return (
+        <div className="container page">
+          <div className="row">
+            {data.map((item) => {
+              return (
+                <div className="col-md-4" key={item.id}>
+                  <div className="thumbnail">
+                    <img
+                      src={item.img}
+                      alt="Nature"
+                      style={{ width: "100%"}}
+                    ></img>
+                  </div>
+                  <Card.Footer>
+                    <div className="caption">
+                      <h6 style={{ color: "blue" }}>
+                      
+                      </h6>
+                      <h6>
+                        {item.name}
+                      </h6>
+                      <p>{item.description}</p>
+                      <button type="button" className="btn btn-primary btn-block">RFQ </button>
+                    </div>
+                  </Card.Footer>
+                </div>
+              );
+            })}
+          </div>
+         
+        </div>
+      );
+  }
+ export default Home;
+
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/usersdata', function(req, res, next) {
+router.get('/products', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.json({"name":"Mukesh",
-"email":"xyz@gmail.com"})
+  res.json([{id:1,name:"Bajaj immerson Rod",img:"https://images-na.ssl-images-amazon.com/images/I/81KdpwazROL._SL1500_.jpg",partnumber:"IMRBJ1500",description:"Now you don’t need to brave the cold water in the winters to take a bath if you have this immersion rod from Bajaj at your disposal.",seller:"AB Group",stock:46000},
+  {id:2,name:"TRIAK",img:"https://4.imimg.com/data4/QQ/OW/GLADMIN-179588/triac-500x500.jpg",partnumber:"TRIAK3PCH",description:"VDRM: 600V IT(AV): 4A Case: T0220 Commonly used in: Light dimmers, Washing machines, Motor control, Hand tools etc. Gate trigger current:6-15mA (VAK=12V ,RL=100ohm)",seller:"GM inc",stock:67890},
+  {id:3,name:"ACCL",img:"https://images-na.ssl-images-amazon.com/images/I/61MGKtHfvyL._SL1000_.jpg",partnumber:"ACCL1PHLT",description:"Elmeasure 32A 1Ph ACCL (Automatic Phase Changeover with Current Limiter) White Colour with Display",seller:"Larcen&Tubro",stock:2316433},
+  ])
 });
 
 module.exports = router;
